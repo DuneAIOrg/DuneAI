034e13a9fd7b0214d3a5a361cc8be66238d23002
Synqat <ken@slinky.link>
2024-10-01 10:51:27 -0600

    feat: deploy action + cleanup

--- dist/modules/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOT = exports.COT = exports.LAMBDA = void 0;
-exports.LAMBDA = 'λ';
-exports.COT = 'chain of thought';
-exports.TOT = 'tree of thought';
